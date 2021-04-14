12457cba7609ea4863dea26968be502bf7bf12fb
castlop <castlop.ma@gmail.com>
2021-04-14 00:17:24 -0500

    Feat: Add event listener to plus button
    Create a event() function to create multiple event listeners on
    different HTML elements. The first event listener to add  needs
    reference to the plus button and the element which id is #counter:
    
    click 'plus' button ---> modify innerText of counter in HTML
    
    modified:   src/js/controls.js

--- src/js/controls.js
+++ src/js/controls.js
@@ -1,11 +1,24 @@
 import {Counter} from './counter.js';
 
+
+const counterHtml    = document.querySelector('#counter'),
+      plusButton     = document.querySelector('#plus');
+
 let counter;
+
 
 const createCounter = () => {
   return new Counter();
 }
 
+const events = () => {
+  plusButton.addEventListener('click', () => {
+    counter.plusOne();
+    counterHtml.innerText = counter.value;
+  });
+}
+
 export const init = () => {
   counter = createCounter();
+  events();
 }
