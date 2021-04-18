1dd4af77fd3873a2b1c83fb648491e41ddaa8a5a
castlop <castlop.ma@gmail.com>
2021-04-18 15:22:06 -0500

    Feat: Add click event listener to save button
    Click only activates a console.log.
    
    modified:   src/js/toolbar.js

--- src/js/toolbar.js
+++ src/js/toolbar.js
@@ -2,12 +2,18 @@ import {counter} from '../index.js';
 
 
 const resetButton = document.querySelector('#reset-button');
+const saveButton  = document.querySelector('#save-button');
 
 
 const eventsToolbar = () => {
   resetButton.addEventListener('click', () => {
     counter.reset();
   });
+
+  saveButton.addEventListener('click', () => {
+    console.log('Save button');
+  });
+
 }
 
 
