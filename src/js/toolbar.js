90224b6349a5d050ab583e2a2fc0bff175c76b47
castlop <castlop.ma@gmail.com>
2021-04-25 19:30:11 -0500

    Feat: Add Event Listener to save button
    When click on save button, save the value introduced in the input box in
    the Local Storage.
    
    modified:   src/js/toolbar.js

--- src/js/toolbar.js
+++ src/js/toolbar.js
@@ -1,8 +1,10 @@
 import {counter, bgDisabled} from '../index.js';
+import {saveCounter} from './storage';
 import {modalSave} from '../components/modal-save/modal-save';
 
 const resetButton = document.querySelector('#reset-button');
 const saveButton  = document.querySelector('#save-button');
+
 
 
 const eventsToolbar = () => {
@@ -14,6 +16,15 @@ const eventsToolbar = () => {
     bgDisabled.switchDisplay();
     if (!bgDisabled.haveComponent(modalSave)) {
       bgDisabled.appendComponent(modalSave);
+    }
+  });
+
+  modalSave.addEventListener('click', (event) => {
+    const inputField = modalSave.querySelector('input');
+    if (event.target.innerText === 'Save') {
+      saveCounter(inputField.value, counter.value);
+      inputField.value = '';
+      bgDisabled.switchDisplay();
     }
   });
 
