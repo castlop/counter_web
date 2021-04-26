1b5f7176342a2bfc49309b23084cef1240380eee
castlop <castlop.ma@gmail.com>
2021-04-26 01:11:17 -0500

    Feat: Create modal-load component
    modal-load component include files to manage logic and styles. This
    component builds a select element that shows counters loaded from Local
    Storage.
    
    new file:   src/components/modal-load/modal-load.css
    new file:   src/components/modal-load/modal-load.js

---
+++ src/components/modal-load/modal-load.js
@@ -0,0 +1,29 @@
+const createModalLoad = (options) => {
+  
+  const htmlTemplate = `
+    <label for="catalog">Choose one counter</label>
+    <select name="catalog" id="catalog">
+    </select>
+    <button class="button">Load</button>
+    <button class="button">Cancel</button>
+  `;
+  
+  const modalLoad = document.createElement('div');
+  modalLoad.classList.add('modalLoad')
+  modalLoad.innerHTML = htmlTemplate;
+
+  return modalLoad;
+}
+
+
+const createOptionsLoaded = options => {
+  return options
+          .map(option => `<option value="${option.description}">${option.description}: ${option.counterValue}</option>`)
+          .join('\n');
+}
+
+
+export {
+  createModalLoad,
+  createOptionsLoaded
+}
