ba7bc044d1e07458d26244d606dd760653dc9f70
castlop <castlop.ma@gmail.com>
2021-04-26 02:05:34 -0500

    Feat: Add index to modal-load component
    Use index to load modal-load logic for modal creation (including the
    counter list elements) and styles.
    
    new file:   src/components/modal-load/index.js

---
+++ src/components/modal-load/index.js
@@ -0,0 +1,12 @@
+import {createModalLoad, createOptionsLoaded} from './modal-load';
+
+import './modal-load.css';
+
+
+const modalLoad = createModalLoad();
+
+
+export {
+  modalLoad,
+  createOptionsLoaded
+}
