0062718d12f544ce8e4c4d4f99fb0f5c3458e2b7
castlop <castlop.ma@gmail.com>
2021-04-28 02:31:14 -0500

    Feat: Add methods to control display of background
    Add one method to know if the background-disabled component is hidden or
    not and the other one add an event listener to 'force' switch displaying
    and remove appended elements when click on this component.
    
    modified:   src/components/background-disabled/background-disabled.js

--- src/components/background-disabled/background-disabled.js
+++ src/components/background-disabled/background-disabled.js
@@ -4,8 +4,10 @@ class BgDisabled {
     this._itemHtml    = document.createElement('div');
     this._classHidden = classes.hidden;
     this._itemHtml.classList.add(...Object.values(classes));
+    
+    this._loadHardTurnOffEvent();
   }
-  
+    
   set itemHtml(newItem) {
     throw 'Cannot assign other HTML element';
   }
@@ -30,8 +32,21 @@ class BgDisabled {
     return this._itemHtml.contains(component);
   }
 
+  isHidden() {
+    return this._itemHtml.classList.contains(this._classHidden);
+  }
+  
   switchDisplay() {
     this._itemHtml.classList.toggle(this._classHidden);
+  }
+
+  _loadHardTurnOffEvent() {
+    this._itemHtml.addEventListener('click', (event) => {
+      if (event.target === this._itemHtml) {
+        this.switchDisplay();
+        this.clearComponents();
+      }
+    });
   }
 
 }
