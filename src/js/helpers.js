00dd5bcc2db2e2610719ca91304e9b3d32d62cfb
castlop <castlop.ma@gmail.com>
2021-04-30 01:35:26 -0500

    Feat: Extract numeric value from string
    modified:   src/js/helpers.js

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -2,7 +2,13 @@ const createId = () => {
   return (new Date()).getTime();
 }
 
+const extractCounterValue = (counterValueAsString) => {
+  const substrings = counterValueAsString.split(':', 10);
+  return parseInt(substrings.pop());
+}
+
 
 export {
   createId,
+  extractCounterValue
 }
